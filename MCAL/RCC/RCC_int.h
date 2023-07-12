8a88cf2b4d81e394829923c288b27507af45bfae
Ahmed-Osman-01 <ahmed.osman0072@gmail.com>
2023-07-12 08:57:01 +0300

    Added 2 functions for enabling and disabling peripherals

--- MCAL/RCC/RCC_int.h
+++ MCAL/RCC/RCC_int.h
@@ -1,6 +1,37 @@
+/****************************************************************************/
+/* AUTHOR               :     Ahmed Osman                                   */
+/* Origin Date          :     11/07/2023                                    */
+/* Version              :     1.0.0                                         */
+/* SWC                  :     RCC                                           */
+/****************************************************************************/
+
 #ifndef RCC_INT_H
 #define RCC_INT_H
 
+typedef enum
+{
 
+    RCC_AHB_ID,
+    RCC_APB2_ID,
+    RCC_APB1_ID,
+
+}RCC_BUSS_ID_t;
+
+/****************************************************************************/
+/* Enables the clock of specified peripheral.                               */
+/*                                                                          */
+/* Choose one of the three busses (AHB, APB2, APB1) ID and the pin          */
+/* corresponding to the desired perihperal as defined in STM32F103C8 file   */
+/****************************************************************************/
+void RCC_enablePeripheral(RCC_BUSS_ID_t copy_enBuss, u8 copy_u8Peripheral);
+
+
+/****************************************************************************/
+/* Disables the clock of specified peripheral.                              */
+/*                                                                          */
+/* Choose one of the three busses (AHB, APB2, APB1) ID and the pin          */
+/* corresponding to the desired perihperal as defined in STM32F103C8 file   */
+/****************************************************************************/
+void RCC_disablePeripheral(RCC_BUSS_ID_t copy_enBussID, u8 copy_u8Peripheral);
 
 #endif
--- MCAL/RCC/RCC_prog.c
+++ MCAL/RCC/RCC_prog.c
@@ -14,3 +14,46 @@
 #include "RCC_priv.h"
 #include "RCC_config.h"
 
+/**
+ * @brief Enables the clock of specified peripheral
+ * 
+ * @param copy_enBussID The ID of the buss containing the peripheral
+ * @param copy_u8Peripheral The pin of the peripheral as defined in the STM32F103C8 file
+ */
+void RCC_enablePeripheral(RCC_BUSS_ID_t copy_enBussID, u8 copy_u8Peripheral)
+{
+    if(copy_enBussID == RCC_AHB_ID)
+    {
+        SET_BIT(RCC->RCC_AHBENR, copy_u8Peripheral);
+    }
+    else if(copy_enBussID == RCC_APB2_ID)
+    {
+        SET_BIT(RCC->RCC_APB2ENR, copy_u8Peripheral);
+    }
+    else if(copy_enBussID == RCC_APB1_ID)
+    {
+        SET_BIT(RCC->RCC_APB1ENR, copy_u8Peripheral);
+    }
+}
+
+/**
+ * @brief Disables the clock of specified peripheral
+ * 
+ * @param copy_enBussID The ID of the buss containing the peripheral
+ * @param copy_u8Peripheral The pin of the peripheral as defined in the STM32F103C8 file
+ */
+void RCC_disablePeripheral(RCC_BUSS_ID_t copy_enBussID, u8 copy_u8Peripheral)
+{
+    if(copy_enBussID == RCC_AHB_ID)
+    {
+        CLR_BIT(RCC->RCC_AHBENR, copy_u8Peripheral);
+    }
+    else if(copy_enBussID == RCC_APB2_ID)
+    {
+        CLR_BIT(RCC->RCC_APB2ENR, copy_u8Peripheral);
+    }
+    else if(copy_enBussID == RCC_APB1_ID)
+    {
+        CLR_BIT(RCC->RCC_APB1ENR, copy_u8Peripheral);
+    }
+}
